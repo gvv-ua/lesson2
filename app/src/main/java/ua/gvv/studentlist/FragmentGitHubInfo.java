dfa9349fc90f4718ef89331692d8e2ba13d0d66d
Gvv <mail.gvv@gmail.com>
2016-11-04 19:59:55 +0200

    GitHub button

--- app/src/main/java/ua/gvv/studentlist/FragmentGitHubInfo.java
+++ app/src/main/java/ua/gvv/studentlist/FragmentGitHubInfo.java
@@ -2,10 +2,7 @@ package ua.gvv.studentlist;
 
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -18,11 +15,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
@@ -89,7 +86,8 @@ public class FragmentGitHubInfo extends Fragment implements Callback {
         View root = getView();
 
         ImageView avatar = (ImageView) root.findViewById(R.id.git_hub_user_avatar);
-        new DownLoadImageTask(avatar).execute(gitHubUser.getAvatarUrl());
+//        new DownLoadImageTask(avatar).execute(gitHubUser.getAvatarUrl());
+        Picasso.with(root.getContext()).load(gitHubUser.getAvatarUrl()).transform(new CropCircleTransformation()).into(avatar);
 
         TextView view = (TextView) root.findViewById(R.id.git_hub_user_name);
         view.setText(gitHubUser.getName());
@@ -124,27 +122,27 @@ public class FragmentGitHubInfo extends Fragment implements Callback {
         client.newCall(request).enqueue(this);
     }
 
-    private class DownLoadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView imageView;
-
-        public DownLoadImageTask(ImageView imageView) {
-            this.imageView = imageView;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urlOfImage = urls[0];
-            Bitmap logo = null;
-            try {
-                InputStream is = new URL(urlOfImage).openStream();
-                logo = BitmapFactory.decodeStream(is);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return logo;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            imageView.setImageBitmap(result);
-        }
-    }
+//    private class DownLoadImageTask extends AsyncTask<String, Void, Bitmap> {
+//        ImageView imageView;
+//
+//        public DownLoadImageTask(ImageView imageView) {
+//            this.imageView = imageView;
+//        }
+//
+//        protected Bitmap doInBackground(String... urls) {
+//            String urlOfImage = urls[0];
+//            Bitmap logo = null;
+//            try {
+//                InputStream is = new URL(urlOfImage).openStream();
+//                logo = BitmapFactory.decodeStream(is);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            return logo;
+//        }
+//
+//        protected void onPostExecute(Bitmap result) {
+//            imageView.setImageBitmap(result);
+//        }
+//    }
 }
