e7f471bb7387d17f06fad0ba100f4de84c4ce812
Gvv <mail.gvv@gmail.com>
2016-11-26 23:27:11 +0200

    implements contact loader

--- app/src/main/java/ua/gvv/studentlist/FragmentContactList.java
+++ app/src/main/java/ua/gvv/studentlist/FragmentContactList.java
@@ -1,16 +1,28 @@
 package ua.gvv.studentlist;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by gvv on 26.11.16.
  */
 
-public class FragmentContactList extends Fragment {
+public class FragmentContactList extends Fragment  implements LoaderManager.LoaderCallbacks<Cursor> {
+    private final static int CONTACT_LOADER_ID = 100;
+    private Uri contactUri;
+    private String[] projection;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -18,4 +30,53 @@ public class FragmentContactList extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(CONTACT_LOADER_ID, null, this);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //contactUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
+        projection = new String[]
+                {
+                        ContactsContract.Contacts._ID,
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
+                        ContactsContract.Contacts.LOOKUP_KEY,
+                        ContactsContract.Contacts.PHOTO_THUMBNAIL_URI
+                };
+
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        CursorLoader cursorLoader = null;
+        if (id == CONTACT_LOADER_ID) {
+            cursorLoader = new CursorLoader(getActivity(),
+                    ContactsContract.Contacts.CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    null);
+        }
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if ((data != null) && (data.moveToFirst())) {
+            String name = data.getString(data.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
+            TextView textViewview = (TextView) getActivity().findViewById(R.id.contact_list_title);
+            textViewview.setText(name);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
